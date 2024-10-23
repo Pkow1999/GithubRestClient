e3f71dd15006df3d5d01abc74859645a8dde1f95
Pkow <pkow1999@gmail.com>
2024-10-23 16:47:55 +0200

    Code cleanup + minor change in RestErrorHandler to return status code in json

--- src/main/java/com/example/GithubRestClient/repositories/RepositoriesErrorRecord.java
+++ src/main/java/com/example/GithubRestClient/repositories/RepositoriesErrorRecord.java
@@ -1,7 +1,2 @@
-package com.example.GithubRestClient.repositories;
-
-public record RepositoriesErrorRecord(
-        Integer status,
-        String message
-) {
+package com.example.GithubRestClient.repositories;public class RepositoriesErrorRecord {
 }
